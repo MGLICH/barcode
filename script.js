c45a84b6b7664e191daa7d5c847fddcfab6d2511
Glitch (hello-webpage) <none>
2021-04-15 11:16:25 +0000

    🏓🐃 Checkpoint ./script.js:963655/1283

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+const barcodeDetector = new BarcodeDetector();
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  const stream = await getUserMedia({video:true});
+  const videoTrack = stream.getVideoTracks()[0];
+  const trackProcessor = new TrackProcessor(videoTrack);
+  const trackGenerator = new TrackGenerator();
+  const transformer = new TransformStream({
+     async transform(videoFrame, controller) {
+        const barcodePosition = detectBarcode(videoFrame);
+        let newFrame = blurBackground(videoFrame.data, facePosition);
+        videoFrame.close();
+        controller.enqueue(newFrame);
+    }
+  });
+
+  // After this, trackGenerator can be assigned to any sink such as a
+  // peer connection, or media element.
+  trackProcessor.readable
+      .pipeThrough(transformer)
+      .pipeTo(trackGenerator.writable);
+
+  // Forward Web-exposed signals to the original videoTrack.
+  trackGenerator.readableControl.pipeTo(trackProcessor.writableControl);
