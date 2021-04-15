825878f441850756842aac5732918bd1e251f47d
Glitch (hello-webpage) <none>
2021-04-15 11:28:23 +0000

    🏐🌁 Checkpoint ./script.js:963655/2455 ./index.html:963655/1831

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 const barcodeDetector = new BarcodeDetector();
 
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-  const stream = await getUserMedia({video:true});
+const highlightBarcode = (frame, barcode) => {
+  return frame;
+};
+
+const button = document.querySelector("button");
+button.addEventListener("click", async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
   const videoTrack = stream.getVideoTracks()[0];
   const trackProcessor = new TrackProcessor(videoTrack);
   const trackGenerator = new TrackGenerator();
   const transformer = new TransformStream({
-     async transform(videoFrame, controller) {
-        const barcodePosition = detectBarcode(videoFrame);
-        let newFrame = blurBackground(videoFrame.data, facePosition);
-        videoFrame.close();
-        controller.enqueue(newFrame);
+    async transform(videoFrame, controller) {
+      const barcode = barcodeDetector.detect(videoFrame);
+      const newFrame = highlightBarcode(videoFrame.data, barcode);
+      videoFrame.close();
+      controller.enqueue(newFrame);
     }
   });
 
   // After this, trackGenerator can be assigned to any sink such as a
   // peer connection, or media element.
   trackProcessor.readable
-      .pipeThrough(transformer)
-      .pipeTo(trackGenerator.writable);
+    .pipeThrough(transformer)
+    .pipeTo(trackGenerator.writable);
 
   // Forward Web-exposed signals to the original videoTrack.
   trackGenerator.readableControl.pipeTo(trackProcessor.writableControl);
+});
