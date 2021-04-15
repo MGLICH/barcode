5ce71aaf57a95b25d2e90c4d10c24c7af5d19947
Glitch (hello-webpage) <none>
2021-04-15 13:36:32 +0000

    🕌🍃 Checkpoint ./script.js:963655/2903

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 const barcodeDetector = new BarcodeDetector({
   // (Optional) A series of barcode formats to search for.
   // Not all formats may be supported on all platforms
-  formats: [    
-    'qr_code',    
-  ]
+  formats: ["qr_code"]
 });
 
 const highlightBarcode = (bitmap, timestamp, barcode) => {
-  return new VideoFrame(bitmap, {timestamp});
+  return new VideoFrame(bitmap, { timestamp });
 };
 
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  const videoTrack = stream.getVideoTracks()[0];  
+  const videoTrack = stream.getVideoTracks()[0];
+  
   const transformer = new TransformStream({
     async transform(videoFrame, controller) {
       const bitmap = await createImageBitmap(videoFrame);
-      const barcode = barcodeDetector.detect(bitmap);
-      const newFrame = highlightBarcode(bitmap, videoFrame.timestamp, barcode);
+      const detectedBarcodes = barcodeDetector.detect(bitmap);
+      const timestamp = videoFrame.timestamp;
+      const newFrame = highlightBarcode(bitmap, timestamp, detectedBarcodes);
       videoFrame.close();
       controller.enqueue(newFrame);
-      newFrame.close()
+      newFrame.close();
     }
   });
+  
   const trackProcessor = new MediaStreamTrackProcessor(videoTrack);
-  const trackGenerator = new MediaStreamTrackGenerator('video');
+  const trackGenerator = new MediaStreamTrackGenerator("video");
   // After this, trackGenerator can be assigned to any sink such as a
   // peer connection, or media element.
   trackProcessor.readable
